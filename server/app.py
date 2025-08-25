ef9225f26f6c1efa59384e8eff4f5224ffac64d0
Chei <cochewaki@gmail.com>
2025-08-25 18:36:49 +0300

    added some resources and changed timezone code in models

---
+++ server/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import models
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///rentals.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate (app, db)
+CORS(app)
+
+@app.route('/api/signup', methods=['POST'])
+
+@app.route('/api/login', methods=['POST'])
+
+@app.route('/api/logout', methods=['POST']
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
