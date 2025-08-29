e6f152ca324ad009188c0ec9234032908637ace3
Valentine Wanjiru <kt116tabitha@gmail.com>
2025-08-29 15:07:42 +0300

    added links to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,8 @@ import PaymentPage from './pages/PaymentPage.jsx';
 import PaymentResultPage from './pages/PaymentResultPage.jsx';
 import RepairRequestFormPage from './pages/RepairRequestFormPage.jsx';
 import NoPage from './pages/NoPage.jsx';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Shell from "./layout/Shell";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import Shell from "./layout/Shell"; // Not used, can be removed
 import HomePage from "./pages/HomePage";
 import DashboardPage from "./pages/DashboardPage";
 import PropertiesListPage from "./pages/PropertiesListPage";
@@ -16,16 +16,75 @@ import LeaseFormPage from "./pages/LeaseFormPage";
 import TenantDashboardPage from "./pages/TenantDashboardPage";
 import VacateFormPage from "./pages/VacateFormPage";
 
-
 export default function App() {
   return (
-    <Shell>
-      <Routes>
-        <Route path="/" element={<LandlordDashboard />} />
+    <div>
+      <BrowserRouter>
+        <nav className="bg-gray-800 p-4 shadow-md">
+          <ul className="flex space-x-6">
+            <li>
+              <Link to="/" className="text-white hover:text-gray-300">
+                Landlord Dashboard (Home)
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/tenant/dashboard"
+                className="text-white hover:text-gray-300"
+              >
+                Tenant Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link to="/dashboard" className="text-white hover:text-gray-300">
+                General Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/properties"
+                className="text-white hover:text-gray-300"
+              >
+                Properties
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/properties/new"
+                className="text-white hover:text-gray-300"
+              >
+                Add Property
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/leases-booking"
+                className="text-white hover:text-gray-300"
+              >
+                Create Lease
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/notifications"
+                className="text-white hover:text-gray-300"
+              >
+                Notifications
+              </Link>
+            </li>
+            <li>
+              <Link to="/payment" className="text-white hover:text-gray-300">
+                Make Payment
+              </Link>
+            </li>
 
-      <Shell>
+          </ul>
+        </nav>
+
         <Routes>
-          <Route path="/" element={<HomePage />} />
+
+          <Route path="/" element={<LandlordDashboard />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/properties" element={<PropertiesListPage />} />
           <Route path="/properties/new" element={<PropertyFormPage />} />
@@ -33,22 +92,24 @@ export default function App() {
           <Route path="/properties/:id/edit" element={<PropertyFormPage />} />
           <Route path="/leases-booking" element={<LeaseFormPage />} />
           <Route path="/tenant/dashboard" element={<TenantDashboardPage />} />
-          <Route path= "/leases/:leaseId/vacate" element={<VacateFormPage />} />
-
+          <Route
+            path="/leases/:leaseId/vacate"
+            element={<VacateFormPage />}
+          />
+          <Route path="/leases/:leaseId" element={<LeaseDetailPage />} />
+          <Route path="/notifications" element={<NotificationsFeed />} />
+          <Route path="/payment" element={<PaymentPage />} />
+          <Route
+            path="/payments/result/:paymentId"
+            element={<PaymentResultPage />}
+          />
+          <Route
+            path="/properties/:propertyId/repair-request"
+            element={<RepairRequestFormPage />}
+          />
+          <Route path="*" element={<NoPage />} />
         </Routes>
-      </Shell>
-
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/leases/:leaseId" element={<LeaseDetailPage />} />
-        <Route path="/notifications" element={<NotificationsFeed />} />
-        <Route path="/payment" element={<PaymentPage />} />
-        <Route path="/payments/result/:paymentId" element={<PaymentResultPage />} />
-        <Route path="/properties/:propertyId/repair-request" element={<RepairRequestFormPage />} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>
-    </Shell>
+      </BrowserRouter>
+    </div>
   );
 }
-
-
-
